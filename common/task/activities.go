4b2a5eb6e2491c2d74b77a844449f5383dd2255a
Samuel Agbonkpolo <samuelagm@gmail.com>
2021-01-02 00:14:36 +0100

    pruned files

--- common/task/activities.go
+++
@@ -1,6 +0,0 @@
-package task
-
-// ScheduleEmails shedules an email for daily update requests
-func ScheduleEmails(taskID string) error {
-	return nil
-}
--- go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/samuelagm/workflow-service
-
-go 1.15
-
-require (
-	github.com/google/uuid v1.1.2
-	go.temporal.io/sdk v1.2.0
-)
